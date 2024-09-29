2aebe487910665d346d708dd084a7ec4e4a40e60
JennySukut <jennysukut@gmail.com>
2024-09-29 19:48:58 +0200

    updated share modal to be sans-video

--- src/components/modals/signupModals/signupIndividual3.tsx
+++ src/components/modals/signupModals/signupIndividual3.tsx
@@ -17,10 +17,13 @@ export default function SignupModalIndividual3() {
       <Dialog.Title className="Title w-full max-w-[50vw] text-center text-xl font-bold">
         how you can help
       </Dialog.Title>
-      <Dialog.Description className="SmallText w-full text-center font-normal italic">
+      {/* <Dialog.Description className="SmallText w-full text-center font-normal italic">
         {`watch this quick video to learn more:`}
+      </Dialog.Description> */}
+      {/* <video src="" className="aspect-video w-full rounded-3xl bg-peach" /> */}
+      <Dialog.Description className="SmallText w-full text-center text-xs italic text-olive sm:text-base">
+        {`The biggest help to our startup is getting fellow humans involved! The more people we have using this job site, the more businesses will be inclined to post here. It would be fantastic if you told your friends + family + fellow job-seekers to sign up!`}
       </Dialog.Description>
-      <video src="" className="aspect-video w-full rounded-3xl bg-peach" />
       <div className="ButtonContainer mt-4 flex flex-col items-center justify-end gap-4 sm:flex-row">
         <Link href={"/support"}>
           <SiteButton
