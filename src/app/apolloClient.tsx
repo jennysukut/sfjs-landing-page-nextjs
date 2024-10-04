348cb7ecf66570a49879e1cae8f851c0749ff71c
Carl Thronson <carlthronson@gmail.com>
2024-10-04 15:03:21 -0700

    Add API key header for graphql API requests

--- src/app/apolloClient.tsx
+++ src/app/apolloClient.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "https://straightforward-job-site-backend-obsy.onrender.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get the API key from an environment variable
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  return {
+    headers: {
+      ...headers,
+      'X-Api-Key': apiKey,
+    }
+  };
+});
 
 const client = new ApolloClient({
-  uri: "https://straightforward-job-site-backend-obsy.onrender.com/graphql",
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default function ApolloWrapper({
--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,10 +1,25 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "https://straightforward-job-site-backend-obsy.onrender.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get the API key from an environment variable
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  return {
+    headers: {
+      ...headers,
+      'X-Api-Key': apiKey,
+    }
+  };
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "https://straightforward-job-site-backend-obsy.onrender.com/graphql",
-  }),
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default client;
