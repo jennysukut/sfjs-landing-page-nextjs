0d695a2ecf8950a2efba17e58ff9cc3670e7e7eb
JennySukut <jennysukut@gmail.com>
2025-01-22 21:43:47 -0700

    added moving elements for all 4 categories of features

--- src/app/our-features/page.tsx
+++ src/app/our-features/page.tsx
@@ -17,6 +17,7 @@ export default function OurFeaturesPage() {
   const [currentDescriptor, setCurrentDescriptor] = useState(descriptorList[0]);
   const [isFlipping, setIsFlipping] = useState(false);
   const [isRotated, setIsRotated] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   const rotate = {
     start: { opacity: 0, rotate: -15 },
@@ -34,6 +35,39 @@ export default function OurFeaturesPage() {
           repeat: 0,
           ease: "easeInOut",
           delay: 1,
+        },
+      },
+    },
+  };
+
+  const swirl = {
+    start: { rotate: 360 },
+    move: {
+      rotate: 0,
+      transition: {
+        rotate: {
+          duration: 10,
+          repeat: Infinity,
+          ease: "easeInOut",
+          delay: 0,
+        },
+      },
+    },
+  };
+
+  const bounce = {
+    start: { y: 100 },
+    move: {
+      y: [
+        100, 0, -20, 0, -20, 5, 50, 0, -20, 0, -20, 5, 50, 0, -20, 0, -20, 5,
+        50, 0, -20, 0, -20, 5, 50, 0,
+      ],
+      transition: {
+        y: {
+          duration: 60,
+          repeat: 0,
+          ease: "easeInOut",
+          delay: 0,
         },
       },
     },
@@ -85,99 +119,160 @@ export default function OurFeaturesPage() {
   }, []);
 
   return (
-    <div className="FeaturesPage flex w-[85%] max-w-[1600px] flex-col justify-center self-center">
-      <div className="TopButtons flex justify-between">
-        <button
-          className="HumanFocusedTechSection items-middle font-mono ml-[2vw] flex gap-3 font-semibold"
-          onClick={() => console.log("human focused tech click")}
-        >
-          <Image
-            width={90}
-            height={90}
-            alt="human focused tech"
-            src="/human-flower.svg"
-            className={`align-middle transition-transform duration-1000 ${isRotated ? "rotate-90" : ""} hover:rotate-45`}
-          ></Image>
-          <div className="Title mt-8 flex flex-col text-left">
-            <p>human</p>
-            <p>focused</p>
-            <p>tech</p>
-          </div>
-        </button>
-        <button
-          className="ApplicationManagerSection items-middle font-mono mr-[2vw] flex gap-3 self-end font-semibold"
-          onClick={() => console.log("application manager click")}
-        >
-          <div className="Title flex flex-col text-right align-middle text-olive">
-            <p>two-way</p>
-            <p>application</p>
-            <p>manager</p>
-          </div>
-          <Image
-            width={80}
-            height={80}
-            alt="two-way application manager"
-            src="/ams-star.svg"
-            className={`align-middle transition-transform duration-1000 ${isRotated ? "-rotate-90" : ""} hover:-rotate-12`}
-          ></Image>
-        </button>
-      </div>
-
-      <div className="TitleSection flex flex-col items-center gap-10 self-center">
-        <h1 className="Title text-[3.5rem] tracking-widest text-midnight">
-          explore our{" "}
-        </h1>
-        <div className="SecondLine -mt-3 mr-6 self-center">
-          <h1
-            className={`Title inline-block font-serif text-[4rem] font-semibold tracking-normal text-midnight`}
-          >
-            <span className="Title font-sans text-[3.5rem] tracking-widest text-midnight">
-              sweet{" "}
-            </span>
-            features
-          </h1>
-        </div>
-      </div>
-
-      <div className="NoteSection flex items-start gap-4 self-center">
-        <div className="Arrow">
-          <motion.div
-            initial="start"
-            variants={rotate}
-            viewport={{ once: true }}
-            whileInView="move"
-            className="align-middle"
+    <div
+      className="LandingPageContainer -mb-24 -mt-48 flex h-[140vh] w-[100vw] max-w-[1600px] flex-col items-center justify-center"
+      style={{
+        backgroundImage: 'url("/BackgroundShapes8.svg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="FeaturesPage flex w-[85%] max-w-[1600px] flex-col justify-center self-center">
+        <div className="TopButtons flex justify-between">
+          <button
+            className="HumanFocusedTechSection items-middle font-mono -mt-20 ml-[15vw] flex gap-3 font-semibold"
+            onClick={() => console.log("human focused tech click")}
           >
             <Image
-              width={60}
-              height={25}
-              alt="arrow"
-              src="/features-arrow.svg"
-              className="ml-4 mt-1 align-middle"
+              width={90}
+              height={90}
+              alt="human focused tech"
+              src="/human-flower.svg"
+              className={`align-middle transition-transform duration-1000 ${isRotated ? "rotate-90" : ""} hover:rotate-45`}
             ></Image>
+            <div className="Title mt-8 flex flex-col text-left">
+              <p>human</p>
+              <p>focused</p>
+              <p>tech</p>
+            </div>
+          </button>
+          <button
+            className="ApplicationManagerSection items-middle font-mono mr-[2vw] flex gap-3 self-end font-semibold"
+            onClick={() => console.log("application manager click")}
+          >
+            <div className="Title flex flex-col text-right align-middle text-olive">
+              <p>two-way</p>
+              <p>application</p>
+              <p>manager</p>
+            </div>
+            <motion.div
+              initial="start"
+              variants={swirl}
+              viewport={{ once: true }}
+              whileInView="move"
+              className="align-middle"
+            >
+              <Image
+                width={80}
+                height={80}
+                alt="two-way application manager"
+                src="/ams-star.svg"
+                className={`align-middle transition-transform duration-1000 ${isRotated ? "-rotate-90" : ""} hover:-rotate-12`}
+              ></Image>
+            </motion.div>
+          </button>
+        </div>
+
+        <div className="TitleSection flex flex-col items-center gap-10 self-center">
+          <h1 className="Title text-[3.5rem] tracking-widest text-midnight">
+            explore our{" "}
+          </h1>
+          <div className="SecondLine -mt-3 mr-6 self-center">
+            <h1
+              className={`Title inline-block font-serif text-[4rem] font-semibold tracking-normal text-midnight`}
+            >
+              <span className="Title font-sans text-[3.5rem] tracking-widest text-midnight">
+                sweet{" "}
+              </span>
+              features
+            </h1>
+          </div>
+        </div>
+
+        <div className="NoteSection flex items-start gap-4 self-center">
+          <div className="Arrow">
+            <motion.div
+              initial="start"
+              variants={rotate}
+              viewport={{ once: true }}
+              whileInView="move"
+              className="align-middle"
+            >
+              <Image
+                width={60}
+                height={25}
+                alt="arrow"
+                src="/features-arrow.svg"
+                className="ml-4 mt-1 align-middle"
+              ></Image>
+            </motion.div>
+          </div>
+          <motion.div
+            initial="start"
+            variants={opacity}
+            viewport={{ once: true }}
+            whileInView="show"
+            className="mt-6 flex gap-2"
+          >
+            <p className="Note font-mono text-midnight">& </p>
+            <div className="RotatingWordContainer w-[17vw]">
+              <span
+                className={`font-mono inline-block text-midnight transition-all duration-1000 ${
+                  isFlipping
+                    ? "-rotate-x-90 -translate-y-2 opacity-0"
+                    : "rotate-x-0 translate-y-0 opacity-100"
+                }`}
+                style={{ transformStyle: "preserve-3d" }}
+              >
+                {currentDescriptor}
+              </span>
+            </div>
           </motion.div>
         </div>
-        <motion.div
-          initial="start"
-          variants={opacity}
-          viewport={{ once: true }}
-          whileInView="show"
-          className="mt-6 flex gap-2"
-        >
-          <p className="Note font-mono text-midnight">& </p>
-          <div className="RotatingWordContainer w-[17vw]">
-            <span
-              className={`font-mono inline-block text-midnight transition-all duration-1000 ${
-                isFlipping
-                  ? "-rotate-x-90 -translate-y-2 opacity-0"
-                  : "rotate-x-0 translate-y-0 opacity-100"
-              }`}
-              style={{ transformStyle: "preserve-3d" }}
+
+        <div className="BottomButtons flex justify-between">
+          <button
+            className="HonestJobBoardSection items-middle font-mono -ml-8 flex gap-4 font-semibold"
+            onClick={() => console.log("active honest job board click")}
+          >
+            <motion.div
+              initial="start"
+              variants={bounce}
+              viewport={{ once: false }}
+              whileInView="move"
+              className="mt-6 flex gap-2"
             >
-              {currentDescriptor}
-            </span>
-          </div>
-        </motion.div>
+              <Image
+                width={175}
+                height={200}
+                alt="active and honest job board"
+                src="/job-board-semi-circle.svg"
+                className={`align-middle transition-transform duration-1000 hover:shrink`}
+              ></Image>
+            </motion.div>
+            <div className="Title mt-8 flex flex-col text-left text-peach">
+              <p>an active</p>
+              <p>& honest</p>
+              <p>job board</p>
+            </div>
+          </button>
+          <button
+            className="NoGhostingSection items-middle font-mono -mt-24 mr-32 flex gap-3 self-end font-semibold"
+            onClick={() => console.log("ghosting deterrent click")}
+          >
+            <div className="Title flex flex-col text-right align-middle text-magenta">
+              <p>our ghosting</p>
+              <p>disincentive</p>
+            </div>
+            <Image
+              width={100}
+              height={100}
+              alt="no more ghosting"
+              src="/ghosting-flower.svg"
+              className={`align-middle transition-transform duration-1000 ${isRotated ? "-rotate-90" : ""} hover:-rotate-12`}
+            ></Image>
+          </button>
+        </div>
       </div>
     </div>
   );
